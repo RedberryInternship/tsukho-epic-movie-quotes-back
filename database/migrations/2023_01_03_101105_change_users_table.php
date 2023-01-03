f96ee374c98b5a122d74545e7632234675a3bf83
giorgi <gcuxishvili@gmail.com>
2023-01-03 14:31:36 +0400

    fix: fix change users table Schemas

--- database/migrations/2023_01_03_101105_change_users_table.php
+++ database/migrations/2023_01_03_101105_change_users_table.php
@@ -11,9 +11,6 @@ return new class extends Migration {
 			$table->dropColumn('email');
 			$table->dropColumn('email_verified_at');
 			$table->dropColumn('password');
-		});
-
-		Schema::table('users', function (Blueprint $table) {
 			$table->string('password')->nullable();
 			$table->string('image');
 			$table->string('google_id')->nullable();
@@ -23,6 +20,12 @@ return new class extends Migration {
 	public function down()
 	{
 		Schema::table('users', function (Blueprint $table) {
+			$table->string('email')->unique();
+			$table->string('email_verified_at')->nullable();
+			$table->string('password');
+			$table->dropColumn('password');
+			$table->dropColumn('image');
+			$table->dropColumn('google_id');
 		});
 	}
 };
