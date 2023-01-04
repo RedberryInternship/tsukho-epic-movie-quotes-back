400feeeb8b31678f617052c93af1c2a0459779a8
giorgi <gcuxishvili@gmail.com>
2023-01-04 13:51:01 +0400

    feat: add ability to receive email in two languages

---
+++ lang/en/email.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'account-verification'   => 'Account Verification',
+	'name'                   => 'Movie Quotes',
+	'joining-text'           => 'Thanks for joining Movie quotes! We really appreciate it. Please click the button below to verify your account:',
+	'verify-button'          => 'Verify account',
+	'clicking'               => 'If clicking doesn`t work, you can try copying and pasting it to your browser:',
+	'any-problem'            => 'If you have any problems, please contact us:',
+	'crew'                   => 'MovieQuotes Crew',
+	'hello'                  => 'Hola',
+];
