329e8a669ee98c5cdeee13deb63be81b2e493c04
giorgi <gcuxishvili@gmail.com>
2023-01-30 20:06:02 +0400

    fix: create tag factory

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TagFactory extends Factory
+{
+	public function definition()
+	{
+		return [
+			'tags' => [
+				'en' => '',
+				'ka' => '',
+			],
+		];
+	}
+}
